5d7fe11714e1ea28f4c72f50ea516b887a442854
iamhruthiks <iamhruthiks@gmail.com>
2025-02-02 20:02:44 +0530

    Section 34: Section 03 - [Baseline project] Integrating H2 database and seed data - 329. S03L03 - Adding repository and service classes in Spring Boot

---
+++ src/main/java/com/example/SpringStarter/services/PostService.java
@@ -0,0 +1,37 @@
+package com.example.SpringStarter.services;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.SpringStarter.models.Post;
+import com.example.SpringStarter.repositories.PostRepository;
+
+@Service
+public class PostService {
+   @Autowired
+   private PostRepository postRepository;
+
+   public Optional<Post> getById(Long id) {
+       return postRepository.findById(id);
+   }
+
+   public List<Post> getAll() {
+       return postRepository.findAll();
+   }
+   
+   public void delete(Post post) {
+       postRepository.delete(post);
+       
+   }
+   
+   public Post save(Post post) {
+       if (post.getId() == null) {
+           post.setCreatedAt(LocalDateTime.now());
+       }
+       return postRepository.save(post);
+   }
+}
