98851f94341b8c1578b33b19265dbf990e2021d9
iamhruthiks <iamhruthiks@gmail.com>
2025-02-02 20:45:28 +0530

    Section 34: Section 03 - [Baseline project] Integrating H2 database and seed data - 331. S03L05 - Displaying data on webpage, Spring Boot

--- src/main/java/com/example/SpringStarter/Controller/HomeController.java
+++ src/main/java/com/example/SpringStarter/Controller/HomeController.java
@@ -1,13 +1,24 @@
 package com.example.SpringStarter.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.SpringStarter.models.Post;
+import com.example.SpringStarter.services.PostService;
+
 @Controller
 public class HomeController {
+    @Autowired
+    private PostService postService;
+
     @GetMapping("/")
-    public String home(Model Model) {
+    public String home(Model model) {
+        List<Post> posts = postService.getAll();
+        model.addAttribute("posts", posts);
         return "home";
     }
 }
