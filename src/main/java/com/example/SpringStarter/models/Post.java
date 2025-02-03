4c0bb704b5e74c38068e4a7dc21b5d6e7468fa75
iamhruthiks <iamhruthiks@gmail.com>
2025-02-03 06:31:07 +0530

    Section 35: Section 04 – Adding user accounts support in the Spring Application - 332. S04L01 - Adding Account models in Spring Boot

--- src/main/java/com/example/SpringStarter/models/Post.java
+++ src/main/java/com/example/SpringStarter/models/Post.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,5 +28,9 @@ public class Post {
    private String body;
 
    private LocalDateTime createdAt;
+   
+   @ManyToOne
+   @JoinColumn(name = "account_id", referencedColumnName = "id", nullable = true )
+   private Account account;
 
 }
