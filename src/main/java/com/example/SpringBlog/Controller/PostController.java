5e29fe58989f39e3cde05cf0ce7a986940a71c46
iamhruthiks <iamhruthiks@gmail.com>
2025-02-10 00:34:43 +0530

    Section 36: Spring Boot: [Baseline project] Add Blog feature into Spring Boot Application - 348. S05L03 - View a post with edit options

--- src/main/java/com/example/SpringBlog/Controller/PostController.java
+++ src/main/java/com/example/SpringBlog/Controller/PostController.java
@@ -1,5 +1,6 @@
 package com.example.SpringBlog.Controller;
 
+import java.security.Principal;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +17,29 @@ public class PostController {
 
     @Autowired
     private PostService postService;
-    @GetMapping("/posts/{id}")
-    public String getPost(@PathVariable Long id,Model model) {
+    @GetMapping("/post/{id}")
+    public String getPost(@PathVariable Long id,Model model, Principal principal) {
         Optional<Post> optionalPost = postService.getById(id);
-
+        String authUser = "email";
         if (optionalPost.isPresent()) {
             Post post = optionalPost.get();
             model.addAttribute("post", post);
+
+            //get username of current logged in session user
+            // String authUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+
+            if (principal != null) {
+                authUser = principal.getName();
+            }
+            if (authUser.equals(post.getAccount().getEmail())) {
+                model.addAttribute("isOwner", true);
+            } else {
+                model.addAttribute("isOwner", false);
+            }
+            
             return "post_views/post";
-        } else {
-            return "404";
-        }
+            } else {
+                return "404";
+            }
     }
 }
