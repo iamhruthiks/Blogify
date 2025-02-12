ef63a823482a42321eac3261cefa3949224bab9e
iamhruthiks <iamhruthiks@gmail.com>
2025-02-12 21:01:35 +0530

    Section 38: Spring Boot: [Baseline project] Add Update account feature - 362. S07L03 - Update Profile Photo on the Spring Boot Blog

--- src/main/java/com/example/SpringBlog/Controller/AccountController.java
+++ src/main/java/com/example/SpringBlog/Controller/AccountController.java
@@ -1,20 +1,31 @@
 package com.example.SpringBlog.Controller;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.SpringBlog.models.Account;
 import com.example.SpringBlog.services.AccountService;
+import com.example.SpringBlog.util.AppUtil;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -28,6 +39,9 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
     
+    @Value("${spring.mvc.static-path-pattern}")
+    private String photo_prefix;
+
     @GetMapping("/register")
     public String register(Model model) {
         Account account = new Account();
@@ -70,7 +84,8 @@ public class AccountController {
 
     @PostMapping("/profile")
     @PreAuthorize("isAuthenticated()")
-    public String post_profile(@Valid @ModelAttribute Account account, BindingResult bindingResult,Principal principal,HttpServletRequest request) {
+    public String post_profile(@Valid @ModelAttribute Account account, BindingResult bindingResult, Principal principal,
+            HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "account_views/profile";
         }
@@ -79,7 +94,7 @@ public class AccountController {
             authUser = principal.getName();
         }
         Optional<Account> optionalAccount = accountService.findOneByEmail(authUser);
-         if (optionalAccount.isPresent()) {
+        if (optionalAccount.isPresent()) {
             Account account_by_id = accountService.findById(account.getId()).get();
             account_by_id.setAge(account.getAge());
             account_by_id.setDate_of_birth(account.getDate_of_birth());
@@ -91,18 +106,64 @@ public class AccountController {
             accountService.save(account_by_id);
             // SecurityContextHolder.clearContext();
             // return "redirect:/";
-            
+
             // Invalidate the session
             HttpSession session = request.getSession(false); // Get the session if it exists
             if (session != null) {
                 session.invalidate();
             }
-           // SecurityContextHolder.clearContext();  No need to clear context after invalidating session
-           return "redirect:/"; // Redirect to the login page with a logout message
+            // SecurityContextHolder.clearContext();  No need to clear context after invalidating session
+            return "redirect:/"; // Redirect to the login page with a logout message
 
         } else {
             return "redirect:/?error";
         }
+    }
+    
+    @PostMapping("/update_photo")
+    @PreAuthorize("isAuthenticated()")
+    public String update_photo(@RequestParam("file") MultipartFile file, RedirectAttributes attributes, Principal principal) {
+        if (file.isEmpty()) {
+            attributes.addFlashAttribute("error", "No file uploaded");
+            return "redirect:/profile";
+        } else {
+            String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+            
+            try {
+                int length = 10;
+                boolean useLetters = true;
+                boolean useNumbers = true;
+                String generatedString = RandomStringUtils.random(length,useLetters,useNumbers);
+                String final_photo_name = generatedString + fileName;
+                String absolute_fileLocation = AppUtil.get_upload_path(final_photo_name);
+
+                Path path = Paths.get(absolute_fileLocation);
+                Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+                attributes.addFlashAttribute("message", "You successfully uploaded");
+
+                String authUser = "emial";
+                if (principal != null) {
+                    authUser = principal.getName();
+                }
+                Optional<Account> optionalAccount = accountService.findOneByEmail(authUser);
+                if (optionalAccount.isPresent()) {
+                    Account account = optionalAccount.get();
+                    Account account_by_id = accountService.findById(account.getId()).get();
+                    String relative_fileLocation = photo_prefix.replace("**", "uploads/" + final_photo_name);
+                    account_by_id.setPhoto(relative_fileLocation);
+                    accountService.save(account_by_id);
+                }
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt();
+                }
+                return "redirect:/profile";
+
+            }catch (Exception e) {
+            }
+        }
+        return "redirect:/profile?error";
     }
     
 }
--- src/main/java/com/example/SpringBlog/Controller/PostController.java
+++ src/main/java/com/example/SpringBlog/Controller/PostController.java
@@ -20,7 +20,6 @@ import com.example.SpringBlog.services.PostService;
 
 import jakarta.validation.Valid;
 
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
