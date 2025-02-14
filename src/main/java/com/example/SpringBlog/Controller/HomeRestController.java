8bed72b87b5483cdadf3bf34068e19c82bd13fa8
iamhruthiks <iamhruthiks@gmail.com>
2025-02-14 11:24:28 +0530

    Section 42: Spring Boot: Spring boot Logging - 374. S11L01 - Spring Boot Logging

--- src/main/java/com/example/SpringBlog/Controller/HomeRestController.java
+++ src/main/java/com/example/SpringBlog/Controller/HomeRestController.java
@@ -2,6 +2,8 @@ package com.example.SpringBlog.Controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +17,13 @@ import com.example.SpringBlog.services.PostService;
 public class HomeRestController {
     @Autowired
     private PostService postService;
+    
+    Logger logger = LoggerFactory.getLogger(HomeRestController.class);
 
     @GetMapping("/")
-    public List<Post> home(){  
-        return postService.findAll();
+    public String home() {
+        logger.error("this is a test error log");
+        return "sample response";
     }
 
 }
